87388a15e83c9a4d5706c5c7e141d1a1c7594710
´jollan77´ <jollan77>
2023-09-12 17:43:28 +0200

    provade

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-  test 3
-    <TheWelcome />
-  </main>
+    <div id="myDiv" class="header">
+<h2>My to do list</h2>
+<input type="text" id="myInput" placeholder="Titel...">
+<span :onclick="'newElement()'" class="addBtn">Add</span>
+    </div>
+
+<ul id="myUL">
+ <li>Lära mig programmera</li> 
+ <li>Kunna resa</li>
+ <li class="checked">Äta god mat</li>
+</ul>
 </template>
